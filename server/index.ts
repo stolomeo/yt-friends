7dba051fec483e15ef3e04b256bc8ec7ca4bc723
stolomeo <tolomeosebastian@gmail.com>
2022-06-01 23:49:15 -0400

    Test server

---
+++ server/index.ts
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET, POST"],
+  },
+});
+
+server.listen(3000, () => {
+  console.log("Listening...");
+});
