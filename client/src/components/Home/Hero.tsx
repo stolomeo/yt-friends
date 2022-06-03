58d0d82ef1956d28c2dcfdd0f226711a06c9042c
stolomeo <tolomeosebastian@gmail.com>
2022-06-03 01:00:51 -0400

    Created hero section

--- client/src/components/Home/Hero.tsx
+++ client/src/components/Home/Hero.tsx
@@ -1,5 +1,50 @@
-import React from "react";
+import styled from "styled-components";
 
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <HeroWrapper>
+      <InfoSection>
+        <Text>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos
+          assumenda soluta, neque nesciunt deserunt suscipit et unde voluptatem,
+          in eveniet mollitia dicta? Rerum sit vitae qui quas ducimus asperiores
+          perferendis?
+        </Text>
+        <Button>Get Started</Button>
+      </InfoSection>
+      <ImageSection>
+        <Image />
+      </ImageSection>
+    </HeroWrapper>
+  );
 }
+
+const HeroWrapper = styled.section`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 4rem 6rem;
+`;
+
+const InfoSection = styled.section`
+  width: 50%;
+`;
+
+const Text = styled.h2`
+  line-height: 1.5;
+  font-size: 32px;
+`;
+
+const Button = styled.button`
+  width: 250px;
+  height: 80px;
+  font-size: 40px;
+`;
+
+const ImageSection = styled.section``;
+
+const Image = styled.img`
+  background-color: aquamarine;
+  width: 600px;
+  height: 300px;
+`;
--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,11 +1,18 @@
+import styled from "styled-components";
 import Header from "./Header";
 import Hero from "./Hero";
 
 export default function Home() {
   return (
-    <>
+    <HomeWrapper>
       <Header />
       <Hero />
-    </>
+    </HomeWrapper>
   );
 }
+
+const HomeWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
